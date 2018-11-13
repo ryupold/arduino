08134cb9261d5baa99076e4bb6676b0011537dce
Michael Scherbakow <ryupold@gmail.com>
2018-11-14 00:36:28 +0100

    showing sensor data & uptime

--- smarden/smarden.ino
+++ smarden/smarden.ino
@@ -4,9 +4,6 @@ const int DRY = 820;   //x >= DRY  means the sensor is completly dry
 const int WATER = 485; // x <= WATER means the sensor is submerged in water
 const int MOIST_SOIL = 700;
 int sensorPin = A3;
-const int avrg_len = 100;
-int avrg[avrg_len];
-int avrg_index = 0;
 
 void setup()
 {
